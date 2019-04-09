ec6636df86b62d7af297ed92bc41940ed154a674
Alexander Vershilov <alexander.vershilov@tweag.io>
2019-04-09 16:53:13 +0300

    Certificate generation in jcli.

--- src/bin/jcli_app/certificate/mod.rs
+++ src/bin/jcli_app/certificate/mod.rs
@@ -1,12 +1,11 @@
-use bech32::{Bech32, FromBase32, ToBase32};
+use bech32::{Bech32, FromBase32};
 use cardano::util::hex;
-use chain_core::{property::Serialize, mempack::{Readable, ReadBuf}};
-use chain_addr::{AddressReadable};
-use chain_crypto::{bech32, AsymmetricKey, PublicKey, Ed25519Extended, SecretKey, FakeMMM, Curve25519_2HashDH};
+use chain_core::property::Serialize;
+use chain_crypto::{bech32, Ed25519Extended, PublicKey, SecretKey};
 use chain_impl_mockchain::{
-    certificate::{self, CertificateContent, StakeKeyRegistration, StakeDelegation,  StakeKeyDeregistration},
-    stake::{StakeKeyId, StakePoolId, StakePoolInfo},
+    certificate::{self, CertificateContent},
     leadership::genesis::GenesisPraosLeader,
+    stake::{StakeKeyId, StakePoolInfo},
 };
 use std::{fs, path::PathBuf};
 use structopt::StructOpt;
@@ -16,23 +15,12 @@ use structopt::StructOpt;
 pub enum Certificate {
     /// Build transaction and write it to stdout as hex-encoded message
     New(NewArgs),
-    //Decode(DecodeArgs),
 }
 
 #[derive(StructOpt)]
 #[structopt(rename_all = "kebab-case")]
 pub enum NewArgs {
-    // StakeKeyRegistration(StakeKeyRegistrationArgs),
-    // StakeKeyDeregistration(StakeKeyDeregistrationArgs),
-    // StakeDelegation(StakeDelegationArgs),
     StakePoolRegistration(StakePoolRegistrationArgs),
-//    StakePoolRetirement(StakePoolRetirementArgs),
-}
-
-#[derive(StructOpt)]
-pub struct DecodeArgs {
-    #[structopt(name = "CERTIFICATE_FILE")]
-    input: Option<String>,
 }
 
 #[derive(StructOpt)]
@@ -61,10 +49,9 @@ pub struct StakeDelegationArgs {
     pub private_key: PathBuf,
 }
 
-#[derive(Debug)]
-#[derive(StructOpt)]
+#[derive(Debug, StructOpt)]
 pub struct StakePoolRegistrationArgs {
-    #[structopt(long = "serial", name="SERIAL")]
+    #[structopt(long = "serial", name = "SERIAL")]
     pub serial: u128,
     #[structopt(long = "owner", name = "PUBLIC_KEY")]
     pub owners: Vec<String>,
@@ -79,115 +66,26 @@ pub struct StakePoolRegistrationArgs {
 impl NewArgs {
     pub fn exec(self) {
         match self {
-            //NewArgs::StakeKeyRegistration(args) =>
-            //    args.exec(),
-            //NewArgs::StakeKeyDeregistration(args) =>
-            //    args.exec(),
-            //NewArgs::StakeDelegation(args) =>
-            //    args.exec(),
-            NewArgs::StakePoolRegistration(args) =>
-                args.exec(),
+            NewArgs::StakePoolRegistration(args) => args.exec(),
         }
     }
 }
 
-/*
-impl StakeKeyRegistrationArgs {
-
-    pub fn exec(self) {
-        let bech32: Bech32 = self.key.parse().unwrap();
-        let stake_key_id = if bech32.hrp() == Ed25519Extended::PUBLIC_BECH32_HRP {
-            let pub_key_bytes = Vec::<u8>::from_base32(bech32.data()).unwrap();
-            PublicKey::from_binary(&pub_key_bytes).unwrap()
-        } else {
-            panic!("Invalid key type, received {} but was expecting {}",
-                   bech32.hrp(),
-                   Ed25519Extended::PUBLIC_BECH32_HRP
-                   )
-        };
-
-        let content = StakeKeyRegistration {
-            stake_key_id: StakeKeyId::from(stake_key_id)
-        };
-        let cert = certificate::Certificate {
-            content: CertificateContent::StakeKeyRegistration(content),
-            // signatures: vec![]
-        };
-        // let priv_key = SecretKey::from_bytes(&bytes) 
-        let data = cert.serialize_as_vec().unwrap();
-        println!("{}", hex::encode(&data));
-    }
-}
-
-impl StakeKeyDeregistrationArgs {
-
-    pub fn exec(self) {
-        let bech32: Bech32 = self.key.parse().unwrap();
-        let stake_key_id = if bech32.hrp() == Ed25519Extended::PUBLIC_BECH32_HRP {
-            let pub_key_bytes = Vec::<u8>::from_base32(bech32.data()).unwrap();
-            PublicKey::from_binary(&pub_key_bytes).unwrap()
-        } else {
-            panic!("Invalid key type, received {} but was expecting {}",
-                   bech32.hrp(),
-                   Ed25519Extended::PUBLIC_BECH32_HRP
-                   )
-        };
-
-        let content = StakeKeyDeregistration {
-            stake_key_id: StakeKeyId::from(stake_key_id)
-        };
-        let cert = certificate::Certificate {
-            content: CertificateContent::StakeKeyDeregistration(content),
-            // signatures: vec![]
-        };
-        // let priv_key = SecretKey::from_bytes(&bytes) 
-        let data = cert.serialize_as_vec().unwrap();
-        println!("{}", hex::encode(&data));
-    }
-}
-
-impl StakeDelegationArgs {
-    fn exec(self) -> () {
-        let bech32: Bech32 = self.key.parse().unwrap();
-        let stake_key_id = if bech32.hrp() == Ed25519Extended::PUBLIC_BECH32_HRP {
-            let pub_key_bytes = Vec::<u8>::from_base32(bech32.data()).unwrap();
-            PublicKey::from_binary(&pub_key_bytes).unwrap()
-        } else {
-            panic!("Invalid key type, received {} but was expecting {}",
-                   bech32.hrp(),
-                   Ed25519Extended::PUBLIC_BECH32_HRP
-                   )
-        };
-
-        let bytes = hex::decode(&self.pool_id).unwrap();
-        let mut bytes = ReadBuf::from(&bytes);
-        let pool_id = StakePoolId::read(&mut bytes).unwrap();
-
-        let content = StakeDelegation {
-            stake_key_id: StakeKeyId::from(stake_key_id),
-            pool_id,
-        };
-        let cert = certificate::Certificate {
-            content: CertificateContent::StakeDelegation(content),
-            // signatures: vec![]
-        };
-        // let priv_key = SecretKey::from_bytes(&bytes) 
-        let data = cert.serialize_as_vec().unwrap();
-        println!("{}", hex::encode(&data));
-    }
-}
-*/
-
 impl StakePoolRegistrationArgs {
     pub fn exec(self) {
+        let owners = self
+            .owners
+            .iter()
+            .map(|key| {
+                let stake_key_id =
+                    <PublicKey<Ed25519Extended> as bech32::Bech32>::try_from_bech32_str(key)
+                        .unwrap();
+                StakeKeyId::from(stake_key_id)
+            })
+            .collect();
 
-        let owners = self.owners.iter().map(|key| {
-            let stake_key_id = <PublicKey<Ed25519Extended> as bech32::Bech32>::try_from_bech32_str(key).unwrap();
-            StakeKeyId::from(stake_key_id)
-        }).collect();
-
-        let input_str = fs::read_to_string(&self.kes_key)
-            .expect("Cannot read input key from the given input");
+        let input_str =
+            fs::read_to_string(&self.kes_key).expect("Cannot read input key from the given input");
         let bech32: Bech32 = input_str
             .trim_end()
             .parse()
@@ -195,8 +93,8 @@ impl StakePoolRegistrationArgs {
         let pub_key_bytes = Vec::<u8>::from_base32(bech32.data()).unwrap();
         let kes_public_key = PublicKey::from_binary(&pub_key_bytes).unwrap();
 
-        let input_str = fs::read_to_string(&self.vrf_key)
-            .expect("Cannot read input key from the given input");
+        let input_str =
+            fs::read_to_string(&self.vrf_key).expect("Cannot read input key from the given input");
         let bech32: Bech32 = input_str
             .trim_end()
             .parse()
@@ -215,20 +113,27 @@ impl StakePoolRegistrationArgs {
             initial_key,
         };
 
-        let signatures = self.priv_keys.iter().map(|key_path| {
-            let key_str = fs::read_to_string(key_path).unwrap();
-            let private_key = <SecretKey<Ed25519Extended> as bech32::Bech32>::try_from_bech32_str(&key_str.trim()).unwrap();
-            content.make_certificate(&private_key)
-        }).collect();
+        let signatures = self
+            .priv_keys
+            .iter()
+            .map(|key_path| {
+                let key_str = fs::read_to_string(key_path).unwrap();
+                let private_key =
+                    <SecretKey<Ed25519Extended> as bech32::Bech32>::try_from_bech32_str(
+                        &key_str.trim(),
+                    )
+                    .unwrap();
+                content.make_certificate(&private_key)
+            })
+            .collect();
 
         let cert = certificate::Certificate {
             content: CertificateContent::StakePoolRegistration(content),
-            signatures
+            signatures,
         };
 
         let data = cert.serialize_as_vec().unwrap();
         println!("{}", hex::encode(&data));
-
     }
 }
 
@@ -236,8 +141,6 @@ impl Certificate {
     pub fn exec(self) {
         match self {
             Certificate::New(args) => args.exec(),
-            // Certificate::Decode(args) => args.exec(),
         }
     }
 }
-
